9b4efdc46c9f04b911b001717eaf35fb9d703ee1
iJrM <44652718+iJrM@users.noreply.github.com>
2018-12-13 01:11:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+const client3 = new Discord.Client();
 
 client.on('ready', () => {
    console.log(`----------------`);
@@ -16,49 +17,67 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-    if(message.content === '-راتب'){
+    if(message.content === '-2'){
         message.channel.send('#daily')
     }
 });
 
 client.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
+    if(message.content === '-2'){
+        message.channel.send('#credits  ! SToReD.#0241 10000')
     }
 });
 
 client.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
+    if(message.content === '-2'){
+        message.channel.send('#rep  ! SToReD.#0241')
     }
 });
 
 client2.on('message', message => {
-    if(message.content === '-راتب'){
+    if(message.content === '-2'){
         message.channel.send('#daily')
     }
 });
 
 client2.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
+    if(message.content === '-2'){
+        message.channel.send('#credits ! SToReD.#0241 10000')
     }
 });
 
 client2.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
+    if(message.content === '-2'){
+        message.channel.send('#rep ! SToReD.#0241')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '-2'){
+        message.channel.send('#daily ! SToReD.#0241')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '-2'){
+        message.channel.send('#credits ! SToReD.#0241 10000')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '-2'){
+        message.channel.send('#rep ! SToReD.#0241')
     }
 });
 
 
 
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
+if (message.content === '!spam2') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**1**[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -68,11 +87,25 @@ if (message.content === '!spam') {
 });
 
 client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
+if (message.content === '!spam2') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**2**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client3.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam2') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**3**[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -86,3 +119,4 @@ if (message.content === '!spam') {
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
+client3.login(process.env.TOKEN3);
